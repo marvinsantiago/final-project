147aef43c98eee05ee806cd8fd655949f65e329d
Marvin Santiago <marvin.santiago@ymail.com>
2015-09-02 19:46:41 -0500

    CRUD for activities and events

--- app/activities/show/route.js
+++ app/activities/show/route.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params) {
+    return this.store.findById('activity', params.id);
+  },
+  actions: {
+    delete: function(activity) {
+      activity.destroyRecord();
+
+      this.transitionTo('activities.index');
+    }
+  }
 });
