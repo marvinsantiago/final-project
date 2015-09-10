5a412e8ddeded33756150dcb6cc114028bc208af
Marvin Santiago <marvin.santiago@ymail.com>
2015-09-09 20:44:36 -0500

    Update login function, if statement when user not authenicating. Also moved signup function within the login form

--- app/login/route.js
+++ app/login/route.js
@@ -2,11 +2,21 @@ import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'simple-auth/mixins/unauthenticated-route-mixin';
 
 export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  firebase: Ember.inject.service(),
   actions: {
     login(email, password) {
-      this.get('session').authenticate('authenticator:firebase', {
+      this.get('firebase').authWithPassword({
         email,
-        password
+        password,
+      }, (err) => {
+        if (err) {
+          window.alert('The username or password you entered was incorrect', err);
+      } else {
+        this.get('session').authenticate('authenticator:firebase', {
+          email,
+          password
+        });
+      }
       });
     }
     }
