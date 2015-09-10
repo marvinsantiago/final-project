95571229b559c743b0df73155ca3df119655555e
Marvin Santiago <marvin.santiago@ymail.com>
2015-09-10 15:59:03 -0500

    Work on more styling within app

--- app/activities/show/route.js
+++ app/activities/show/route.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(params) {
-    return this.store.findById('activity', params.id);
-  },
   actions: {
     delete: function(activity) {
       if (window.confirm('Are you sure you want to delete?')) {
