2dc687577e90345606b29920a654cdf827de9a7b
Marvin Santiago <marvin.santiago@ymail.com>
2015-09-10 13:15:59 -0500

    Add sevice categories to use for dropdown option for categories

--- app/components/activity-form/component.js
+++ app/components/activity-form/component.js
@@ -1,15 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    resetFormValues: function() {
+  resetFormValues: function() {
     var values = this.get('activity').toJSON();
     this.setProperties(values);
   }.on('init'),
+
+  categories: Ember.inject.service(),
 
   actions: {
     save: function() {
       var data = this.getProperties('name', 'category', 'location', 'phone', 'url');
       this.sendAction('save', this.get('activity'), data);
-    }
+    },
+    selectionsChanged: function() {
+      var selectCategory = this.getProperties('category');
+      this.sendAction('selectionsChanged', this.get('activity'), selectCategory);
+    },
   }
 });
