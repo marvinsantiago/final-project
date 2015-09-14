6d7e9094a1ba1ed912c86cb925ce0a95f62273c8
Marvin Santiago <marvin.santiago@ymail.com>
2015-09-14 15:23:32 -0500

    Correct calendar issue not saving correct date, also filter dates on event index

--- app/components/event-form/component.js
+++ app/components/event-form/component.js
@@ -10,7 +10,9 @@ export default Ember.Component.extend({
   actions: {
     save: function() {
       var data = this.getProperties('title', 'description', 'location', 'start', 'end', 'price', 'url');
-            data.start = moment(data.start);
+      data.start = moment(data.start);
+      data.end = moment(data.end);
+
       this.sendAction('save', this.get('event'), data);
     }
   }
