920d4c51efbb3763742b6fb9fc13c81a4c1c7926
Marvin Santiago <marvin.santiago@ymail.com>
2015-09-16 10:05:34 -0500

    Worked on mobile view footer, added padding

--- app/components/categories-selection/component.js
+++ app/components/categories-selection/component.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  categories: [],
-
   categories: Ember.computed('categories', 'categories.@each.category', 'categories.@each.name', function() {
     return this.get('categories').reduce((build, activity) => {
       var obj = build.findBy('name', activity.get('category'));
